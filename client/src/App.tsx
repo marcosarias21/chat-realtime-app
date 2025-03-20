e57a1c18fd75b795d6ebd331416ff028e6033089
marcosarias21 <marcosariasolaiz@gmail.com>
2025-03-20 09:12:03 -0300

    Implemented functional chat general

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,31 +2,35 @@ import { io } from 'socket.io-client'
 import { useEffect, useState } from 'react'
 import { Chat } from './components/Chat'
 import './App.css'
+import { MessageChat } from './types/types.d'
 
 const socket = io("http://localhost:3000")
 
 const App = () => {
+  const [messageChat, setMessageChat] = useState<MessageChat[]>([])
+  console.log(messageChat)
   useEffect(() => {
     socket.on("connect", () => {
       console.log("Conected to server w/ ID:", socket.id)
     });
 
-    socket.on("chat_message", (data) => {
-      console.log("Message:", data)
+    socket.on("chat_message", (message) => {
+      setMessageChat(messageChat => [...messageChat, message])
     });
 
     return () => {
       socket.off("connect")
+      socket.off("chat_message")
     };
   }, [])
   
   return (
     <section className='h-dvh flex flex-col justify-center items-center w-full gap-4 text-gray-700'>
       <div>
-        <h1 className='text-2xl font-bold'>Talking</h1>
+        <h1 className='text-2xl font-bold'>General Chat</h1>
       </div>
       <div className='h-100 w-100'>
-        <Chat />
+        <Chat socket={socket} messageChat={messageChat}/>
       </div>
     </section>
   )
--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,12 +1,34 @@
-const Chat = () => {
+import { useMessageStore } from "../../store/messageStore"
+import { MessageChat } from "../../types/types.d"
+
+interface Prop {
+  socket: any
+  messageChat: MessageChat[]
+}
+
+const Chat: React.FC<Prop> = ({ socket, messageChat }) => {
+  console.log(messageChat)
+  const { setMessage, message  } = useMessageStore()
+  const sendMessage  = () => {
+    socket.emit("chat_message", message, socket.id)
+    setMessage("")
+  }
+
   return (
     <div className="border-1 h-full border-gray-300 flex flex-col items-center justify-between rounded">
-      <div>
-        Mostrar mensajes
+      <div className="w-full p-2">
+        {messageChat?.map((message, index) => 
+        <div className={`flex mb-1 ${message.user != socket.id && "justify-end text-gray-700" }`} key={index}>
+          <div className={`bg-blue-500 font-medium py-3 px-3  flex ${message.user != socket.id ? 'flex-row-reverse rounded-l-2xl rounded-t-2xl bg-gray-300 text-gray-700': "text-white/90 rounded-t-2xl rounded-r-2xl"}`}>
+            <p>{message.user == socket.id ? "Me:" : `:${message.user}`}</p>
+            <p>{message.content}</p>
+          </div>
+        </div>
+        )}
       </div>
       <div className="w-full flex">
-        <input className="font-medium border-gray-400 py-3 px-4 block w-full border text-sm focus:border-blue-400 focus:ring-1 focus:ring-blue-400 focus:outline-none dark:text-white dark:placeholder-gray-400" type="text" placeholder="Enter message" />
-        <button className="text-white px-2 bg-blue-400">Enviar</button>
+        <input className="font-medium border-gray-400 py-3 px-4 block w-full border text-sm focus:border-blue-400 focus:ring-1 focus:ring-blue-400 focus:outline-none dark:text-white dark:placeholder-gray-400" type="text" placeholder="Enter message" onChange={({ target }) => setMessage(target.value)} value={message} />
+        <button onClick={sendMessage} className="text-white px-2 bg-blue-400">Enviar</button>
       </div>
     </div>
   )
