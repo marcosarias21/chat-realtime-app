7534328a2d6d883e0758fbeb92517649326b958f
marcosarias21 <marcosariasolaiz@gmail.com>
2025-04-07 10:56:37 -0300

    Chat room created

--- client/src/pages/Room/Room.tsx
+++ client/src/pages/Room/Room.tsx
@@ -1,22 +1,33 @@
 import { RoomComponent } from '@/components/RoomComponent'
+import { useAuthStore } from '@/store/authStore'
 import { useSocketState } from '@/store/socketStore'
-import { useEffect } from 'react'
+import { ChatRoomActual, UserChat } from '@/types/types.d'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const Room = () => {
   const { socket } = useSocketState()
   const { id } = useParams()
-  console.log(id)
+  const { user } = useAuthStore()
+  const [roomData, setRoomData] = useState<ChatRoomActual>()
+  const userFiltered: UserChat[] | undefined = roomData?.users?.filter(
+    (u) => u.username != user?.username,
+  )
+
   useEffect(() => {
     socket.emit('joinRoom', id)
     socket.on('room_created', (data) => {
-      console.log(data)
+      setRoomData(data)
     })
-  }, [])
+    return () => {
+      socket.off('room_created')
+    }
+  }, [socket, id])
+
   return (
-    <div className="flex h-dvh w-full flex-col items-center justify-center gap-2">
-      <h2>Room w/user</h2>
-      <RoomComponent />
+    <div className="container mx-auto flex h-dvh w-full flex-col items-center justify-center gap-2">
+      <h2>Chat w/ {userFiltered && userFiltered[0].username}</h2>
+      <RoomComponent contentChat={roomData?.message} />
     </div>
   )
 }
