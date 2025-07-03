2fc4cac8344c0834017bb4da33c464efc5da6d88
marcosarias21 <marcosariasolaiz@gmail.com>
2025-07-03 11:16:40 -0300

    Create custom hook to refactor socket functions on App.tsx

---
+++ client/src/hooks/useSocketHandler.tsx
@@ -0,0 +1,40 @@
+import { useChatStore } from '@/store/chatStore'
+import { useSocketState } from '@/store/socketStore'
+import { ChatRoomType, User } from '@/types/types.d'
+import { useEffect } from 'react'
+
+const useSocketHandler = (user: User | null) => {
+  const { socket } = useSocketState()
+  const { setUsersOnline } = useChatStore()
+
+  const { setChatAvailable } = useChatStore()
+
+  useEffect(() => {
+    socket.emit('user_logged', user?._id)
+    socket.on('users_connected', (users) => {
+      setUsersOnline(users)
+    })
+
+    socket.on('disconnect', () => {
+      console.log('Desconectado')
+    })
+
+    socket.on('chat_available', (chats: ChatRoomType[]) => {
+      console.log(chats)
+      if (chats) {
+        const chatsFiltered = chats?.map((chat) => ({
+          ...chat,
+          users: chat.users.find((c) => c.username != user?.username),
+        }))
+        setChatAvailable(chatsFiltered)
+      }
+    })
+    return () => {
+      socket.off('user_logged')
+      socket.off('users_connected')
+      socket.off('chat_available')
+    }
+  }, [socket, user, location.pathname])
+}
+
+export default useSocketHandler
